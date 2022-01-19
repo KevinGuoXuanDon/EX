6f2b2c5d9ad38760bf19af55c66c8079955df5b9
Xuandong Guo <2427543166@qq.com>
2022-01-19 12:11:06 +0000

    update day13

---
+++ Algorithm/src/day13SearchAndBacktracking/PathInMatrix12.java
@@ -0,0 +1,50 @@
+package day13SearchAndBacktracking;
+
+/**
+ * 定一个m x n 二维字符网格board 和一个字符串单词word.
+ * 如果word 存在于网格中，返回 true ；否则，返回 false.
+ * 单词必须按照字母顺序，通过相邻的单元格内的字母构成，其中“相邻”单元格是那些水平相邻或垂直相邻的单元格。
+ * 同一个单元格内的字母不允许被重复使用。
+ *
+ *  * 例如，在下面的 3×4 的矩阵中包含单词 "ABCCED"（单词中的字母已标出）。
+ *  *
+ * 输入：board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+ * 输出：true
+ */
+public class PathInMatrix12 {
+
+    //5ms 60.62%
+    //40.1MB 55.47
+    private int[][] flag;
+    public boolean exist(char[][] board, String word) {
+        flag = new int[board.length][board[0].length];
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if(board[i][j]==word.charAt(0) && recur(board,word,0,i,j)) return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean recur(char[][]board, String word, int length, int i, int j){
+        //限制边界,找出边界了就直接返回flase
+        if(i<0 || i>=board.length || j<0 || j>=board[0].length) return false;
+        //这个位置已经用了，防止重复查找
+        if(flag[i][j]==1) return false;
+        //当前值不匹配
+        if(board[i][j]!=word.charAt(length)) return false;
+        //如果上面的特殊情况都没有触发，此时当前站点的点也是字符中的一个，标记当前点,进入下一步
+        else{
+            flag[i][j]=1;
+            length++;
+            if(length==word.length()) return true;
+            if (recur(board,word,length,i+1,j)) return true;
+            if (recur(board,word,length,i-1,j)) return true;
+            if (recur(board,word,length,i,j+1)) return true;
+            if (recur(board,word,length,i,j-1)) return true;
+        }
+        //如果找不到就返回flase
+        flag[i][j]=0;
+        return false;
+    }
+}
---
+++ Algorithm/src/day13SearchAndBacktracking/RobotMovingRange13.java
@@ -0,0 +1,42 @@
+package day13SearchAndBacktracking;
+
+/**
+ * 地上有一个m行n列的方格，从坐标 [0,0] 到坐标 [m-1,n-1] 。
+ * 一个机器人从坐标 [0, 0] 的格子开始移动，它每次可以向左、右、上、下移动一格（不能移动到方格外），
+ * 不能进入行坐标和列坐标的数位之和大于k的格子。例如，当k为18时，机器人能够进入方格 [35, 37] ，因为3+5+3+7=18。
+ * 但它不能进入方格 [35, 38]，因为3+5+3+8=19。请问该机器人能够到达多少个格子？
+ *
+ * 输入：m = 2, n = 3, k = 1
+ * 输出：3
+ */
+
+
+public class RobotMovingRange13 {
+    public int movingCount(int m, int n, int k) {
+        int[][] flag = new int[m][n];
+        return recur(flag,0,0,k);
+    }
+
+    public int recur(int[][] flag, int i, int j, int k){
+        int m = flag.length;
+        int n = flag[0].length;
+        if(i<0||i>m-1 || j<0 || j>n-1|| getSum(i,j)>k || flag[i][j]==1) return 0;
+        flag[i][j]=1;
+        // 下面写法是1ms 60.74%
+        // return 1+recur(flag, i+1, j, k)+recur(flag, i-1, j, k)+recur(flag, i, j+1, k)+recur(flag, i, j-1, k);
+        // 0ms 100%. 隐藏优化，只需要向右和下找，因为一旦某一个在之前return了，之后这个方向的都会大于K
+        return 1+recur(flag, i+1, j, k)+recur(flag, i, j+1, k);
+    }
+    public int getSum(int i, int j){
+        int result=0;
+        while(i!=0){
+            result+=i%10;
+            i/=10;
+        }
+        while(j!=0){
+            result+=j%10;
+            j/=10;
+        }
+        return result;
+    }
+}
