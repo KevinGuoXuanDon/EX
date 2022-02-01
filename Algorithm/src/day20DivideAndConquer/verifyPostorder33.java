f04bf8be66db7882a913c1a3e95b783091a9bb3f
Xuandong Guo <2427543166@qq.com>
2022-02-01 23:55:55 +0000

    update day20

--- Algorithm/src/day20DivideAndConquer/verifyPostorder33.java
+++ Algorithm/src/day20DivideAndConquer/verifyPostorder33.java
@@ -1,4 +1,22 @@
 package day20DivideAndConquer;
 
-public class verifyPostorder33 {
+ class VerifyPostorder33 {
+    public boolean verifyPostorder (int[] postorder) {
+        return recur(postorder,0, postorder.length-1);
+    }
+    public boolean recur(int[] postorder, int left, int right){
+        if(left>=right) return true;
+        int p = left;
+        while(postorder[p]<postorder[right]) p++;
+        int m = p;
+        while(postorder[p]>postorder[right]) p++;
+        return p == right && recur(postorder, left, m - 1) && recur(postorder, m, right - 1);
+    }
+
+    public static void main(String[] args) {
+        VerifyPostorder33 v = new VerifyPostorder33();
+        int[] a  = new int[]{1,3,2,6,5};
+        boolean b = v.verifyPostorder(a);
+        System.out.println(b);
+    }
 }
