4668eb652bce5c8f7ca1ce15eab3f4eeabb69d49
Xuandong Guo <2427543166@qq.com>
2022-06-15 09:56:18 +0100

    6-14

---
+++ Algorithm/src/leetcode/Stack/ImplementQueueUsingStacks.java
@@ -0,0 +1,44 @@
+package leetcode.Stack;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class ImplementQueueUsingStacks {
+    public class MyQueue() {
+        Deque<Integer> inDeque;
+        Deque<Integer> outDeque;
+
+        public MyQueue() {
+            inDeque = new ArrayDeque<>();
+            outDeque = new ArrayDeque<>();
+        }
+
+        public void push(int x) {
+            inDeque.push(x);
+        }
+
+        public int pop() {
+            if(outDeque.isEmpty()){
+                transfer();    
+            }
+            return outDeque.pop();
+        }
+
+        private void transfer() {
+            while(!inDeque.isEmpty()){
+                outDeque.push(inDeque.pop());
+            }
+        }
+
+        public int peek() {
+            if(outDeque.isEmpty()){
+                transfer();
+            }
+            return outDeque.peek();
+        }
+
+        public boolean empty() {
+            return inDeque.isEmpty() && outDeque.isEmpty();
+        }
+    }
+}
